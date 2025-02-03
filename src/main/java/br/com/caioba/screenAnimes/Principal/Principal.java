17ad02a01cf446799a1437bc1daa5c6fb68acf57
Caio costa <caio-dbz2011@hotmail.com>
2025-02-03 15:57:20 -0300

    novas classes para obter dados de episodios e temporadas

---
+++ src/main/java/br/com/caioba/screenAnimes/Principal/Principal.java
@@ -0,0 +1,40 @@
+package br.com.caioba.screenAnimes.Principal;
+
+import br.com.caioba.screenAnimes.model.DadosAnimes;
+import br.com.caioba.screenAnimes.model.DadosTemporada;
+import br.com.caioba.screenAnimes.service.ConsumoApi;
+import br.com.caioba.screenAnimes.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private ConverteDados converte = new ConverteDados();
+
+    private final  String ENDERECO = "https://www.omdbapi.com/?t=";
+
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibirMenu(){
+        System.out.println("Digite o nome anime: ");
+        var nomeAnime = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeAnime.replace(" ","+") + API_KEY);
+        DadosAnimes dados = converte.obterDados(json, DadosAnimes.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i <= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeAnime.replace(" ","+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = converte.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+    }
+}
--- src/main/java/br/com/caioba/screenAnimes/ScreenAnimesApplication.java
+++ src/main/java/br/com/caioba/screenAnimes/ScreenAnimesApplication.java
@@ -1,5 +1,6 @@
 package br.com.caioba.screenAnimes;
 
+import br.com.caioba.screenAnimes.Principal.Principal;
 import br.com.caioba.screenAnimes.model.DadosAnimes;
 import br.com.caioba.screenAnimes.model.DadosEpisodios;
 import br.com.caioba.screenAnimes.model.DadosTemporada;
@@ -21,24 +22,7 @@ public class ScreenAnimesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Dragon+ball+z&apikey=6585022c");
-
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosAnimes dados = conversor.obterDados(json, DadosAnimes.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=Dragon+ball+z&season=1&episode=2&apikey=6585022c");
-		DadosEpisodios dadosEpi = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpi);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for(int i = 1; i <= dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=Dragon+ball+z&season=" + i + "&apikey=6585022c");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibirMenu();
 	}
 }
